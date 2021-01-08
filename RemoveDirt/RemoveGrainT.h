b56f96f58dd76e45347869072380dc0032f87e8a
Pinterf <pinterfer@gmail.com>
2021-01-08 10:59:09 +0100

    Remove RemoveGrainT from project (moved to RgTools in 2019)

--- RemoveDirt/RemoveGrainT.h
+++
@@ -1,8 +0,0 @@
-#ifndef __REMOVEGRAINT___H__
-#define __REMOVEGRAINT___H__
-
-#include "avisynth.h"
-
-AVSValue __cdecl CreateTemporalRepair(AVSValue args, void* user_data, IScriptEnvironment* env);
-
-#endif	// __REMOVEGRAINT___H__
